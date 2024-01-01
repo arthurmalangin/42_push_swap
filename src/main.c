d3dbd93c1501ae0760c072c13d7f8f3cdab247cc
Arthur <deymons@42.netriv.fr>
2024-01-01 23:06:17 +0100

    Edit parsing

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: amalangi <amalangi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/30 16:50:00 by amalangi          #+#    #+#             */
-/*   Updated: 2024/01/01 20:11:21 by amalangi         ###   ########.fr       */
+/*   Updated: 2024/01/01 23:05:49 by amalangi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -133,7 +133,7 @@ int main(int argc, char **argv)
         ft_putstr_fd("Error\n", 2);
         return (0);
     }
-    fill_stack(&stack, argv);
+    fill_stack(&stack, ft_split(argv[1], ' '));
     sort(&stack, &stack_b);
     return (0);
 }

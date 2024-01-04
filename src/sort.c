09b97c24d0e6eb271da1e5ae7c20bb21543a678d
Arthur <deymons@42.netriv.fr>
2024-01-04 17:52:23 +0100

    Edit Algo

--- src/sort.c
+++ src/sort.c
@@ -6,7 +6,7 @@
 /*   By: amalangi <amalangi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/01 19:46:54 by amalangi          #+#    #+#             */
-/*   Updated: 2024/01/02 20:22:17 by amalangi         ###   ########.fr       */
+/*   Updated: 2024/01/04 17:47:04 by amalangi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,6 +73,18 @@ int get_approach_inferior(t_stack *stack, int value)
 	return (get_inferior_value(stack, get_approach_value(stack, value)));
 }
 
+int get_index(t_stack *stack, int value)
+{
+	int i = 1;
+	while (stack)
+	{
+		if (stack->value == value)
+			return i;
+		stack = stack->next;
+		i++;
+	}
+	return 0;
+}
 
 void sort(t_stack **stack_a, t_stack **stack_b)
 {
@@ -87,10 +99,6 @@ void sort(t_stack **stack_a, t_stack **stack_b)
 	pb(stack_a, stack_b);
 	min_b = find_min(*stack_b);
 	max_b = find_max(*stack_b);
-	if ((*stack_b)->value == min_b)
-	{
-		rb(stack_b);
-	}
 	if (stacksize(*stack_a) == 3)
 	{
 		//ft_printf("stacksize 3\n");
@@ -101,23 +109,30 @@ void sort(t_stack **stack_a, t_stack **stack_b)
 			min_a = find_min(*stack_a);
 			if ((*stack_a)->value == max_a)
 			{
-				ra(stack_a);
+				if ((*stack_b)->value == min_b)
+					rotate_both(stack_a, stack_b);
+				else
+					ra(stack_a);
 				if (!is_sorted(*stack_a))
 					sa(stack_a);
 			}
 			else if ((*stack_a)->value == min_a)
 			{
-				ra(stack_a);
+				if ((*stack_b)->value == min_b)
+					rotate_both(stack_a, stack_b);
+				else
+					ra(stack_a);
 			}
 			else
 			{
+				rb(stack_b);
 				sa(stack_a);
 			}
 		}
-		ft_printf("stacksize 3 end\n");
+		//ft_printf("stacksize 3 end\n");
 		while (*stack_b)
 		{
-			ft_printf("stacksize 3 pb: %d %d\n", get_approach_value(*stack_a, (*stack_b)->value), (*stack_b)->value);
+			//ft_printf("stacksize 3 pb: %d %d\n", get_approach_value(*stack_a, (*stack_b)->value), (*stack_b)->value);
 			if (get_approach_value(*stack_a, (*stack_b)->value) == (*stack_a)->value && (*stack_b)->value < find_max(*stack_a))
 			{
 				pa(stack_a, stack_b);
@@ -130,41 +145,22 @@ void sort(t_stack **stack_a, t_stack **stack_b)
 			}
 			else
 			{
-				ra(stack_a);
+
+				if (get_approach_value(*stack_a, (*stack_b)->value) == get_end_value(*stack_a))
+				{
+					rra(stack_a);
+					pa(stack_a, stack_b);
+				}
+				else
+					ra(stack_a);
 			}
 		}
 		while (!is_sorted(*stack_a))
-			ra(stack_a);
-		
+			if (get_end_value(*stack_a) == find_min(*stack_a))
+				rra(stack_a);
+			else
+				ra(stack_a);
 	}
-	// while (*stack_a)
-	// {
-	// 	if ((*stack_a)->value > max_b)
-	// 	{
-	// 		max_b = (*stack_a)->value;
-	// 		pb(stack_a, stack_b);
-	// 	}
-	// 	else if ((*stack_a)->value < min_b)
-	// 	{
-	// 		min_b = (*stack_a)->value;
-	// 		pb(stack_a, stack_b);
-	// 		rb(stack_b);
-	// 	}
-	// 	else
-	// 	{
-	// 		ra(stack_a);
-	// 		while (get_end_value(*stack_a) < max_b)
-	// 		{
-	// 			pa(stack_a, stack_b);
-	// 			max_b = find_max(*stack_b);
-	// 		}
-	// 		rra(stack_a);
-	// 		max_b = (*stack_a)->value;
-	// 		pb(stack_a, stack_b);
-	// 	}
-	// }
-	// while (*stack_b)
-	// 	pa(stack_a, stack_b);
 }
 
 int get_end_value(t_stack *stack)
